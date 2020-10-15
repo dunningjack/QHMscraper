f2a0c29cc08f0819e0b8d6cca5df375b52f77c73
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-10-15 22:07:30 +0100

    Initial work, returns first instance of vessel containing HMS, need to reiterate

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-from lxml import html
 import requests
-from time import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import datetime
+
+file_name = 'data.txt'
+
+today = datetime.date.today()
+tomorrowdate = datetime.datetime.today() + datetime.timedelta(days=1)
+tomorrow = tomorrowdate.strftime("%d/%m/%y")
+url = 'https://www.royalnavy.mod.uk/qhm/portsmouth/shipping-movements/daily-movements?date=' + tomorrow
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+tables = soup.find("table")
+vessel_list = tables.text
+
+with open(file_name, 'w') as fh:
+    fh.write(vessel_list)
+fh.close()
 
 
-page = requests.get('https://www.royalnavy.mod.uk/qhm/portsmouth/shipping-movements/daily-movements?date=' + datetime.date)
+def get_line_number():
+    with open(file_name) as f:
+        for i, line in enumerate(f, 1):
+            if "HMS" in line:
+                return i - 1
+
+
+
+HMSLine = get_line_number()
+TimeLine = HMSLine - 1
+
+f = open('data.txt')
+lines = f.readlines()
+
+VesselName = lines[HMSLine]
+VesselTime = lines[TimeLine]
+
+print(VesselName, VesselTime)
